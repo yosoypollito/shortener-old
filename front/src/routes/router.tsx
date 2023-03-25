4e1ebd6631377976fbcd043c6de4c285bb50aef2
Daniel Iglesias <dev.daif@gmail.com>
2023-03-25 16:46:37 -0400

    feat: auth routes

--- front/src/routes/router.tsx
+++ front/src/routes/router.tsx
@@ -3,8 +3,14 @@ import { createBrowserRouter } from 'react-router-dom'
 
 //There goes the pages
 import Layout from '@src/layout'
+
+import AuthLayout from '@components/auth/auth.layout'
+
 import Home from '@pages/home'
 import Donation from "@pages/donation"
+
+import Login from "@components/auth/auth.login"
+import Register from "@components/auth/auth.register"
 
 import Error from '@pages/error'
 
@@ -23,8 +29,23 @@ const router = createBrowserRouter([
         element:<Donation/>
       },
       {
+        path:'auth',
+        element:<AuthLayout/>,
+        errorElement:<Error/>,
+        children:[
+          {
+            path:'',
+            element:<Login/>
+          },
+          {
+            path:'register',
+            element:<Register/>
+          },
+        ]
+      },
+      {
         path:'*',
-        element:<Error/>
+        element:<Error/>,
       }
     ]
   }
